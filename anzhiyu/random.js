dc0ad2d2909a462fca5d1ab9f81d142d77b6ad0e
XI <charlie-cty@qq.com>
2025-07-15 19:59:40 +0800

    Site updated: 2025-07-15 19:59:39

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/07/13/hello-world/","2025/07/13/你好，世界/"];function toRandomPost(){
+var posts=["2025/07/13/你好，世界/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
